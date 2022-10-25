d5421fa4b62501de426c9311cf3bf5398e614277
TiyaReba <105912721+TiyaReba@users.noreply.github.com>
2022-10-25 15:37:18 +0530

    final changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
-import { SamplesComponent } from './samples/samples.component';
+// import { SamplesComponent } from './samples/samples.component';
 import { ContactComponent } from './contact/contact.component';
 
+
 const routes: Routes = [
+
    {path:'about',component:AboutComponent},
-   {path:'samples',component:SamplesComponent},
+  //  {path:'samples',component:SamplesComponent},
    {path:'contact',component:ContactComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,28 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FooterComponent } from './footer/footer.component';
-import { AboutComponent } from './about/about.component';
-import { SamplesComponent } from './samples/samples.component';
-import { ContactComponent } from './contact/contact.component';
-import { CarouselModule } from 'ngx-owl-carousel-o';
+// import { FooterComponent } from './footer/footer.component';
+// import { AboutComponent } from './about/about.component';
+// import { SamplesComponent } from './samples/samples.component';
+// import { ContactComponent } from './contact/contact.component';
+// import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
-    AboutComponent,
-    SamplesComponent,
-    ContactComponent,
+    // FooterComponent,
+    // AboutComponent,
+    // SamplesComponent,
+    // ContactComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxSpinnerModule,
-    BrowserAnimationsModule,
-    CarouselModule 
+    // NgxSpinnerModule,
+    // BrowserAnimationsModule,
+    // CarouselModule 
   ],
   providers: [],
   bootstrap: [AppComponent],
